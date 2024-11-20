6a54d3d0b4d2a64a9d3135e675acb4c5dcf787d9
saschazhang@gmail.com <saschazhang@gmail.com>
2024-11-20 11:29:46 +0000

    Edited backend and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,26 +5,46 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        
+        # Dropdown mit Gefängnissen füllen
+        gefaengnisse = anvil.server.call('get_gefaengnisse')
+        self.gefaengnisse_drop_down.items = [(gefaengnis[1], gefaengnis[2]) for gefaengnis in gefaengnisse]
+        
     
-    # Any code you write here will run before the form opens.
-    self.gefaengnisse_drop_down.items = anvil.server.call('get_gefaengnisse')
-    self.label_direktor.text = "Direktor TODO" 
-    self.label_freie_zellen.text = "Freie Zellen TODO"
-    self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}, 
-                                   {'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}]
+        
+    
+        self.repeating_zellen.items = [{'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}, 
+                                       {'zellennummer': 'TODO', 'anzahl_häftlinge': 'TODO'}]
 
-  def gefaengnisse_drop_down_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
+    def gefaengnisse_drop_down_change(self, **event_args):
+        """Dieser Code wird ausgeführt, wenn ein Gefängnis aus dem Dropdown-Menü ausgewählt wird."""
+        # Gefängnis ID und Name aus dem Dropdown-Menü
+        selected_gefaengnis = self.gefaengnisse_drop_down.selected_value
+        gefaengnis_name = selected_gefaengnis[0]  # Name des Gefängnisses
+        gefaengnis_gid = selected_gefaengnis[1]  # GID des Gefängnisses
 
- 
+        # Ruft den Direktor und die Anzahl freier Zellen ab
+        direktor, freie_zellen = anvil.server.call('get_direktor_und_freie_zellen', gefaengnis_gid)
+        
+        # Aktualisiert die Labels mit den abgerufenen Daten
+        self.label_direktor.text = f"Direktor: {direktor}"
+        self.label_freie_zellen.text = f"Freie Zellen: {freie_zellen}"
 
+        # Ruft die Zellen für das ausgewählte Gefängnis ab
+        zellen = anvil.server.call('get_zellen_aus_gefaengnis', gefaengnis_name)
+        
+        # Setzt die Zellen im RepeatingPanel
+        self.repeating_zellen.items = [
+            {'zellennummer': zelle[0], 'anzahl_häftlinge': self.get_anzahl_haeftlinge(zelle[0])} 
+            for zelle in zellen
+        ]
 
-
-  
- 
+    def get_anzahl_haeftlinge(self, zell_id):
+        """Berechnet die Anzahl der Häftlinge in einer Zelle."""
+        # Ruft die Häftlinge für die Zelle ab
+        haeftlinge = anvil.server.call('get_haeftlinge_aus_zelle', zell_id)
+        return len(haeftlinge)
--- server_code/backend.py
+++ server_code/backend.py
@@ -6,6 +6,53 @@ from anvil.files import data_files
 import anvil.server
 import sqlite3
 
+
+
 @anvil.server.callable
 def get_gefaengnisse():
-  return [('TODO 1', 1), ('TODO 2', 2)]
+    with sqlite3.connect(data_files['gefaengnis.db']) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM gefaengnis")
+        gefaengnisse = cursor.fetchall()
+    return gefaengnisse
+
+@anvil.server.callable
+def get_direktor_und_freie_zellen(gefaengnis_gid):
+
+    verwaltung = app_tables.verwaltung.get(GID=gefaengnis_gid)
+    
+    if verwaltung:
+        return verwaltung['Direktor'], verwaltung['Anzahl_an_freien_Zellen']
+    return "Unbekannt", 0  
+
+
+
+# Funktion zum Abrufen der Zellen eines bestimmten Gefängnisses
+@anvil.server.callable
+def get_zellen_aus_gefaengnis(gefaengnis_name):
+ 
+    gefaengnis = app_tables.gefaengnisse.get(Name=gefaengnis_name)
+    if gefaengnis:
+
+        zellen = app_tables.zellen.search(GID=gefaengnis['GID'])
+        return [(zelle['ZID'], zelle['GID']) for zelle in zellen]
+    return [] 
+
+
+
+@anvil.server.callable
+def get_haeftlinge_aus_zelle(zell_id):
+    # Zelle suchen
+    zelle = app_tables.zellen.get(ZID=zell_id)
+    if zelle:
+        # Häftlinge aus der Tabelle 'kann_bewohnen' abrufen
+        kann_bewohnen = app_tables.kann_bewohnen.search(ZID=zelle['ZID'])
+        haeftlinge = [
+            (haeftling['Name'], haeftling['HID']) 
+            for kb in kann_bewohnen 
+            for haeftling in app_tables.haeftlinge.search(HID=kb['HID'])
+        ]
+        return haeftlinge
+    return [] 
+    
+ 
